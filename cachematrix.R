9c5b2fca65d90f14761f5288622ca9e0cf182bed
zhangliliang <zhangll.level0@gmail.com>
2014-05-20 08:26:47 +0800

    Finish the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below can provide a enviroment which store a matrix and
+## its invertion, which can be easily get without duplicative calculation.
 
-## Write a short comment describing this function
+## the makeCacheMatrix function creates a environment to store the matrix and 
+## its invertion. What's more, it provide the get and set method to access to
+## the variables.
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## the cacheSolve function can solve the invertion of matrix x, and store it 
+## to the cache where the invertion can be easily get without duplicative 
+## calculation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
